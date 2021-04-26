1d3078b6c28c4c900e386c652f6c56235cb3607a
talmax1124 <carlitosdiazplaza@gmail.com>
2021-04-26 19:28:03 -0400

    Search Color Change

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -22,7 +22,7 @@ const SearchBox = ({ history }) => {
         placeholder="Search Products"
         className="mr-sm-2 ml-sm-5 srchfld"
       ></Form.Control>
-      <Button type="submit" variant="outline-light" className="p-2 srch">
+      <Button type="submit" variant="primary" className="p-2 srch">
         Search
       </Button>
     </Form>
